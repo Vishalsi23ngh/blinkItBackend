a085125e63516f4da065e7a284845595b89e4af2
Vishal Singh <vishalsingh23101999@gmail.com>
2025-05-29 12:27:13 +0530

    added print statement in springboot application file

--- src/main/java/com/CapStone/blinkitservice/common/EndPoints.java
+++ src/main/java/com/CapStone/blinkitservice/common/EndPoints.java
@@ -2,7 +2,7 @@ package com.CapStone.blinkitservice.common;
 
 public class EndPoints {
 
-    public static String[] publicEndPoints={"/auth/signin","/auth/signup","/category/all","/collections/getActiveCollections","/products/search","/products/details"};
+    public static String[] publicEndPoints={"/", "/auth/signin","/auth/signup","/category/all","/collections/getActiveCollections","/products/search","/products/details"};
 
     public static boolean isPublicEndPoint(String endPoint){
         for (String point:publicEndPoints){
